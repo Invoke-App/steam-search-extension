7a499030bfdcdc93efbe61715d0727f71ee2d798
limited-io <guzman.brandon12@live.com>
2024-11-30 00:26:39 -0700

    fix: no cors

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,7 @@ class SteamSearchExtension {
       return this.steamApps;
     }
     const response = await fetch("https://api.steampowered.com/ISteamApps/GetAppList/v0002/", {
+      mode: "no-cors",
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
       }
--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export default class SteamSearchExtension implements ExtensionInterface {
         }
         // Fetch mimicking a browser request
         const response = await fetch('https://api.steampowered.com/ISteamApps/GetAppList/v0002/', {
+            mode: 'no-cors',
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
             }
